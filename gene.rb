f552b3748183c22ba4a3efb9a31e4ab4ee854c61
Ariadna Villanueva <ariadnavillam@gmail.com>
2021-10-19 09:57:09 -0400

    change gene object

--- gene.rb
+++ gene.rb
@@ -8,13 +8,13 @@ class Gene
     def initialize (params = {})
         gene_id = params.fetch(:Gene_ID, "X000")
             unless gene_id.match(/A[Tt]\d[Gg]\d\d\d\d\d/)
-                puts "Error. Wrong gene ID #{gene_id} in gene information file"
+                puts "Error. Wrong gene ID #{gene_id}."
                 puts 'Gene ID for Arabidopsis Thaliana have the format: /A[Tt]\d[Gg]\d\d\d\d\d/'
                 exit(1)
             else 
                 @Gene_ID = params.fetch(:Gene_ID, "X000")
-                @Gene_name = params.fetch(:Gene_name, "X000")
-                @mutant_phenotype = params.fetch(:mutant_phenotype, "X000")
+                @Gene_name = params.fetch(:Gene_name, "nameX")
+                @mutant_phenotype = params.fetch(:mutant_phenotype, "phenotype")
                 @linked_genes = Array.new
             end
         
@@ -27,11 +27,20 @@ class Gene
     def is_linked(linked_gene)
         if linked_gene.is_a?(Gene)
             @linked_genes.push(linked_gene)
-            puts "#{@Gene_name} linked to #{linked_gene.get_name}."
         else 
             puts "Error. Enter a correct Gene ID."
         end
     end
+
+    def check_linked_genes
+        unless @linked_genes.length == 0
+            @linked_genes.each do |gene|
+                puts "#{@Gene_name} is linked to #{gene.get_name}."
+            end
+        end
+
+    end
+
 
 end
 
