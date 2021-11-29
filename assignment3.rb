44d100d15338cd5bb65a15db25059489bdd44017
ariadnavillam <67317024+ariadnavillam@users.noreply.github.com>
2021-11-29 03:15:19 +0100

    error with backwards motif

--- assignment3.rb
+++ assignment3.rb
@@ -21,13 +21,14 @@ genes.close
 fastaoutput = File.open('./genes.fa', 'w')
 gene_with_exon = Array.new
 
-genearray = ["AT1G22690"]
+#genearray = ["AT1G22690"]
 genearray.each do |geneid|
   
   url = "http://www.ebi.ac.uk/Tools/dbfetch/dbfetch?db=ensemblgenomesgene&format=embl&id=#{geneid}"
   res = fetch(url)  # we really should check that the return value is valid, but... 
   emblfile = Bio::FlatFile.new(Bio::EMBL, StringIO.new(res.body))
   emblfile.each_entry do |entry|
+    motifs = Array.new()
     seq_gen = entry.to_biosequence
     
     entry.features.each do |feature|
@@ -35,39 +36,60 @@ genearray.each do |geneid|
         if feature.position.include? ":" #location in a remote entry
           next
         elsif feature.position.include? "complement"
-          strand = "-"
-          exon_seq = seq_gen.splice(feature.position)
-          match = feature.position.scan(/complement\((\d+..\d+)\)/)
-          ex_ini, ex_fin = match[0].split("..")
+          strand = "-"     
         else
           strand = "+"
-          exon_seq = seq_gen.splice(feature.position)
-          ex_ini, ex_fin = feature.position.split("..")
         end
-
+        exon_seq = seq_gen.splice(feature.position)
+        loc = Bio::Locations.new(feature.position)
+        ex_ini, ex_fin = loc.span
         gene_with_exon.append(geneid)
+
         positions = exon_seq.enum_for(:scan, /(?=(cttctt))/).map { Regexp.last_match.begin(0)} 
         positions.each do |index|
           next if index.nil?
-          loc = "#{ex_ini.to_i+index}..#{ex_ini.to_i+index+5}"
-          if strand == "-"
-            loc = "complement(#{loc})"
+          if strand == "+"
+            loc = "#{ex_ini.to_i+index}..#{ex_ini.to_i+index+5}"
+          else
+            loc = "complement(#{ex_fin.to_i-index-5}..#{ex_fin.to_i-index})"
           end
-          print loc
+          motifs.append([loc,strand,"cttctt"])
         end
+
         positions_comp = exon_seq.enum_for(:scan, /(?=(gaagaa))/).map { Regexp.last_match.begin(0) }
         positions_comp.each do |index|
           next if index.nil?
-          loc = "#{ex_ini.to_i+index}..#{ex_ini.to_i+index+5}"
           if strand == "+"
-            loc = "complement(#{loc})"
+            loc = "#{ex_ini.to_i+index}..#{ex_ini.to_i+index+5}"
+            strand = "-"
+          else
+            loc = "complement(#{ex_fin.to_i-index-5}..#{ex_fin.to_i-index})"
+            strand = "+"
           end
-          print loc
-          puts exon_seq.splice(loc)
+          motifs.append([loc,strand, "gaagaa"])
         end
       end
     
     end
+    motifs = motifs.uniq()
+    motifs.each do |loc,strand,m|
+      print [seq_gen.splice(loc),strand,m] if seq_gen.splice(loc) != m
+      puts
+      strand = "+"
+      strand = "-" if loc.include? "complement"
+      feature = Bio::Feature.new('repeat',Bio::Locations.new(loc))
+      feature.append(Bio::Feature::Qualifier.new('repeat_motif', 'CTTCTT'))
+      feature.append(Bio::Feature::Qualifier.new('strand', strand))
+      entry.features << feature
+    end
+    
+    entry.features.each do |feature|
+      if feature.feature == "repeat"
+        ini,fin = feature.position.span
+        #puts "#{geneid}\t.\t#{feature.assoc["repeat_motif"]}\t#{ini}\t#{fin}\t.\t#{feature.assoc["strand"]}\t.\t"
+      end
+    end
+    
   end
 end
 genes_wo_exon = genearray - gene_with_exon.uniq() 
